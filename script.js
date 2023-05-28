a884130e11129214e9ca4e7b0f1a8e6194eb9ac6
Viktors2 <vitjok22@gmail.com>
2023-05-28 02:46:10 +0200

    script for navbar

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-//Arrow button
 const arrow = document.querySelector(".down");
-const container2 = document.querySelector("container2");
+const resume = document.querySelector(".button-t");
+const navLinks = document.querySelectorAll(".nav-links a");
+const elements = document.querySelectorAll(".c");
+const main = document.querySelector(".con");
+
+resume.addEventListener("click", function () {
+  main.scrollIntoView({ behavior: "smooth" });
+});
+navLinks.forEach((link, index) => {
+  link.addEventListener("click", function () {
+    elements[index + 1].scrollIntoView({ behavior: "smooth" });
+  });
+});
 
 arrow.addEventListener("click", function () {
-  container2.scrollIntoView({ behavior: "smooth" });
+  elements[1].scrollIntoView({ behavior: "smooth" });
 });
