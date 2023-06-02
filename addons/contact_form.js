5b540a25fe2fc99cd8ad0925250fb6d3206c7b6f
Viktors2 <vitjok22@gmail.com>
2023-06-02 16:55:41 +0200

    edited

--- addons/contact_form.js
+++ addons/contact_form.js
@@ -1,68 +1,54 @@
-import { apiKey, apiDomain, mainMail } from './keys.js';
+import { apiKey, apiDomain } from './keys.js';
+
 
 const form = document.querySelector('.c-form'),
-    nameInput = document.querySelector('.c-name'),
-    emailInput = document.querySelector('.c-email'),
-    phoneInput = document.querySelector('.c-phone'),
-    messageInput = document.querySelector('.text'),
-    timeSelect = document.querySelector('.c-time'),
-    sendButton = document.querySelector('.send-email');
-
+        nameInput = document.querySelector('.c-name'),
+        emailInput = document.querySelector('.c-email'),
+        phoneInput = document.querySelector('.c-phone'),
+        messageInput = document.querySelector('.text'),
+        timeInput = document.querySelector('.c-time'),
+        sendButton = document.querySelector('.send-email');
+  
+  
 sendButton.addEventListener('click', sendEmail);
 
-// Function to send email
-function sendMail(data) {
-  return fetch(apiDomain, {
+// To send the email
+function sendEmail(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get values
+  const name = nameInput.value,
+        email = emailInput.value,
+        phone = phoneInput.value,
+        time = timeInput.value,
+        message = messageInput.value;
+
+  // Construct the email data
+  const data = {
+    from: email,
+    to: 'vitjok22@gmail.com',
+    subject: 'Contact Form',
+    text: `Name: ${name}\nEmail: ${email}\nTime: ${time}\nPhone: ${phone}\nMessage: ${message}`
+  };
+
+  // Mailgun API
+  fetch(apiDomain, {
     method: 'POST',
     headers: {
       'Authorization': `Basic ${btoa(`api:${apiKey}`)}`,
     },
     body: new URLSearchParams(data)
   })
-  .then(response => {
-    if (response.ok) {
-      return true;
-    } else {
-      return false;
-    }
-  })
-  .catch(error => {
-    return false;
-  });
-}
-
-// send the email
-function sendEmail(event) {
-  event.preventDefault(); // Prevent form submission
-
-  // values
-  const name = nameInput.value,
-        email = emailInput.value,
-        phone = phoneInput.value,
-        message = messageInput.value,
-        time = timeSelect.value;
-
-  // data for user
-  const userData = {
-    from: email,
-    to: email,
-    subject: 'Reservation Confirmation',
-    text: `Dear ${name},\n\nThank you for making a reservation. Your reservation is confirmed for ${time}.\n\nBest regards,\nAnd have a nice day!`
-  };
-
-  // data for the mainUser
-  const reservationData = {
-    from: email,
-    to: mainMail,
-    subject: 'New Reservation',
-    text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nReservation Time: ${time}\nMessage: ${message}`
-  };
-
-  // Send email to user 
-  Promise.all([sendMail(userData), sendMail(reservationData)])
-    .then(([userEmailSent, reservationEmailSent]) => {
-      if (userEmailSent && reservationEmailSent) {
-        form.reset(); // Reset form
+    .then(response => {
+      if (response.ok) {
+        console.log('Sended!');
+        // Reset the form after successful submission
+        form.reset();
+      } else {
+        console.log('Error blat.');
       }
+    })
+    .catch(error => {
+      console.log('Slomalos(:', error);
     });
 }
