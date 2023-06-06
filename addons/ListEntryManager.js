35ce55cb26c7ea8a8780ee56003b1bb6c26eca11
Viktors2 <vitjok22@gmail.com>
2023-06-06 17:09:29 +0200

    name and email fields added in reviews

--- addons/ListEntryManager.js
+++ addons/ListEntryManager.js
@@ -17,42 +17,42 @@
     const form_data = new FormData(form);
     form.reset();
     const data = {};
-
+  
     for (const field_name of form_data.keys()) {
-      const field = form.querySelector('[name="' + field_name + '"]'); //[name="comment"]
+      const field = form.querySelector('[name="' + field_name + '"]');
       const field_value = form_data.get(field_name).trim();
       if (field.required && field_value.length == 0) {
         return;
       }
       data[field_name] = field_value;
     }
-
+  
     let id = null;
     if (this.callbacks.hasOwnProperty('beforeCreateHook')) {
       id = this.callbacks.beforeCreateHook(form_data);
     }
-
+  
     this.addListElement(data, id);
   }
-
+  
   addListElement(data, id = null) {
     const new_el = this.template.content.firstElementChild.cloneNode(true);
     if (id !== null) {
       new_el.dataset.id = id;
     }
-
+  
     for (const field_name in data) {
       const place = new_el.querySelector('.place_' + field_name);
       if (place !== null) {
         place.textContent = data[field_name];
-        
       }
     }
-
+  
     this.template.parentElement.prepend(new_el);
-
+  
     if (this.callbacks.hasOwnProperty('afterAddToListHook')) {
       this.callbacks.afterAddToListHook(new_el, data);
     }
   }
+  
 }
